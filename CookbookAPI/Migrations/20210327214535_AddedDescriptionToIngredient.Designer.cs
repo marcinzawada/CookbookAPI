cf524928440d9510ec2f4acfd3be823d3ff1e1cc
Marcin <ziewa91@wp.pl>
2021-03-27 23:21:55 +0100

    Added description to Ingredient entity

--- CookbookAPI/Migrations/20210327214535_AddedDescriptionToIngredient.Designer.cs
+++ CookbookAPI/Migrations/20210327214535_AddedDescriptionToIngredient.Designer.cs
@@ -4,14 +4,16 @@ using CookbookAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookbookAPI.Migrations
 {
     [DbContext(typeof(CookbookDbContext))]
-    partial class CookbookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210327214535_AddedDescriptionToIngredient")]
+    partial class AddedDescriptionToIngredient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CookbookAPI/Migrations/20210327214535_AddedDescriptionToIngredient.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookbookAPI.Migrations
+{
+    public partial class AddedDescriptionToIngredient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Ingredients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Ingredients");
+        }
+    }
+}
