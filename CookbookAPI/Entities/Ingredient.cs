2be23033e0c3da24381668825ad573799fcd0834
Marcin <ziewa91@wp.pl>
2021-03-27 23:22:22 +0100

    Added description

--- CookbookAPI/Entities/Ingredient.cs
+++ CookbookAPI/Entities/Ingredient.cs
@@ -13,6 +13,8 @@ namespace CookbookAPI.Entities
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public List<RecipeIngredient> RecipeIngredient { get; set; } = new List<RecipeIngredient>();
     }
 }
