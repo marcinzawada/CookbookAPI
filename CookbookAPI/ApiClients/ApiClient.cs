27df882b9e0bf54f9ab9da5c556c25db3a487d4e
Marcin <ziewa91@wp.pl>
2021-03-27 23:23:31 +0100

    Added wrapper to rest client

---
+++ CookbookAPI/ApiClients/ApiClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CookbookAPI.ApiClients.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+
+namespace CookbookAPI.ApiClients
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly IRestClient _client;
+        private string _baseUrl;
+
+        public ApiClient(IRestClient client, string baseUrl)
+        {
+            _client = client;
+            _baseUrl = baseUrl;
+            _client.BaseUrl = new Uri(_baseUrl);
+        }
+
+        public ApiClient(IRestClient client)
+        {
+            _client = client;
+        }
+
+        public void ChangeBaseUrl(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+            _client.BaseUrl = new Uri(_baseUrl);
+        }
+
+        public async Task<IRestResponse> RequestAsync(string endpoint,
+            Dictionary<string, string> parameters = null, Method method = Method.GET)
+        {
+            if (string.IsNullOrEmpty(_baseUrl))
+                throw new Exception("You have to set base url");
+
+            var request = new RestRequest(endpoint, method);
+
+            if (parameters != null)
+            {
+                foreach (var p in parameters)
+                {
+                    request.AddParameter(p.Key, p.Value);
+                }
+            }
+
+            var response = await _client.ExecuteAsync(request);
+            return response;
+        }
+
+        public async Task<List<TDto>> DownloadAllIResources<TDto>(string resourcesUrl)
+        {
+            var response = await RequestAsync(resourcesUrl);
+
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var dtos = JsonConvert.DeserializeObject<List<TDto>>(response.Content, serializerSettings);
+
+            return dtos;
+        }
+    }
+}
---
+++ CookbookAPI/ApiClients/Interfaces/IApiClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace CookbookAPI.ApiClients.Interfaces
+{
+    public interface IApiClient
+    {
+        public void ChangeBaseUrl(string baseUrl);
+
+        public Task<IRestResponse> RequestAsync(string endpoint,
+            Dictionary<string, string> parameters = null, Method method = Method.GET);
+
+        public Task<List<TDto>> DownloadAllIResources<TDto>(string resourcesUrl);
+
+    }
+}
