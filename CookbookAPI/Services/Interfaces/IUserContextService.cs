cd2384b37e94d7f2993d6e9e257508102da63f60
Marcin <ziewa91@wp.pl>
2021-03-28 13:12:14 +0200

    Added UserContextService

---
+++ CookbookAPI/Services/Interfaces/IUserContextService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Services.Interfaces
+{
+    public interface IUserContextService
+    {
+        public ClaimsPrincipal User { get; }
+        public int? GetUserId { get; }
+        public string GetUserRole { get; }
+    }
+}
---
+++ CookbookAPI/Services/UserContextService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace CookbookAPI.Services
+{
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserContextService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public ClaimsPrincipal User => _httpContextAccessor.HttpContext?.User;
+
+        public int? GetUserId =>
+            User is null ? null : (int?)int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        public string GetUserRole =>
+            User?.FindFirst(x => x.Type == ClaimTypes.Role)?.Value;
+    }
+}
