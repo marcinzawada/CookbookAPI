1044aa7654bd44e6e1d0b64153912953637582c5
Marcin <ziewa91@wp.pl>
2021-03-29 00:01:05 +0200

    Fixed a typo

--- CookbookAPI/Requests/Validators/RegisterRequestValidator.cs
+++ CookbookAPI/Requests/Validators/RegisterRequestValidator.cs
@@ -31,7 +31,7 @@ namespace CookbookAPI.Requests.Validators
                 .Matches("[a-z]").WithMessage("'{PropertyName}' must contain one or more lowercase letters.")
                 .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits.")
                 .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]")
-                .WithMessage("'{ PropertyName}' must contain one or more special characters.")
+                .WithMessage("'{PropertyName}' must contain one or more special characters.")
                 .Matches("^[^£# “”]*$")
                 .WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.");
 
