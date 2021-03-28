c077d9fbd3bc90991e6dcdfb93e218fb20691186
Marcin <ziewa91@wp.pl>
2021-03-28 23:09:25 +0200

    Added JwtGenerator

---
+++ CookbookAPI/Common/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Entities;
+
+namespace CookbookAPI.Common.Interfaces
+{
+    public interface IJwtGenerator
+    {
+        public string Generate(User user);
+    }
+}
---
+++ CookbookAPI/Common/JwtGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CookbookAPI.Common.Interfaces;
+using CookbookAPI.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CookbookAPI.Common
+{
+    public class JwtGenerator : IJwtGenerator
+    {
+        private readonly AuthenticationSettings _authenticationSettings;
+
+        public JwtGenerator(AuthenticationSettings authenticationSettings)
+        {
+            _authenticationSettings = authenticationSettings;
+        }
+
+        public string Generate(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+
+            var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer,
+                _authenticationSettings.JwtIssuer,
+                claims,
+                expires: expires,
+                signingCredentials: cred);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
