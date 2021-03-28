6b4e4d2b080c4843c81289d310853e8400eac1eb
Marcin <ziewa91@wp.pl>
2021-03-28 23:13:52 +0200

    Added Login and Register methods

---
+++ CookbookAPI/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Requests.Account;
+using CookbookAPI.Services.Interfaces;
+
+namespace CookbookAPI.Controllers
+{
+    [Route("/api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            await _accountService.RegisterUser(request);
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var vm = await _accountService.Login(request);
+
+            return Ok(vm);
+        }
+    }
+}
