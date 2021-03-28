af9ac3cf544c2578347de6907d4c50e221368e7f
Marcin <ziewa91@wp.pl>
2021-03-28 23:10:41 +0200

    Added IAccountService

---
+++ CookbookAPI/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Requests.Account;
+using CookbookAPI.ViewModels.Account;
+
+namespace CookbookAPI.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task RegisterUser(RegisterRequest request);
+
+        public Task<LoginVm> Login(LoginRequest request);
+    }
+}
