ab5988177a40ba18b4184161bd75246091ac9b20
Marcin <ziewa91@wp.pl>
2021-03-28 23:12:12 +0200

    Added UserRepository

---
+++ CookbookAPI/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Entities;
+using CookbookAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookbookAPI.Repositories
+{
+    public class UserRepository : BaseRepository<User, CookbookDbContext>
+    {
+        public UserRepository(CookbookDbContext context) : base(context)
+        {
+
+        }
+    }
+}
