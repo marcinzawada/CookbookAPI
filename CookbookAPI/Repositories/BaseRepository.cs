54e26b26a4e08bad821496202e3fb710f7171562
Marcin <ziewa91@wp.pl>
2021-03-28 22:44:27 +0200

    Added BaseRepository

---
+++ CookbookAPI/Repositories/BaseRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookbookAPI.Repositories
+{
+    public abstract class BaseRepository<TEntity, TContext> : IBaseRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected readonly CookbookDbContext _context;
+
+        protected BaseRepository(CookbookDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<TEntity> Add(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Delete(int id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+            if (entity == null)
+            {
+                return entity;
+            }
+
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> Get(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ CookbookAPI/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
+    }
+}
