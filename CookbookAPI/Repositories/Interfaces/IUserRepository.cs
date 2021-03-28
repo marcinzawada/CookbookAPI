0baf6d84bb8d461053264fdac3eb02d7955061f0
Marcin <ziewa91@wp.pl>
2021-03-28 23:34:50 +0200

    Added IUserRepository

---
+++ CookbookAPI/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Entities;
+
+namespace CookbookAPI.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmail(string email);
+    }
+}
