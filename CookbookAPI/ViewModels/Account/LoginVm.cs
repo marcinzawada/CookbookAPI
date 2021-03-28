2f2dfe403a2be97acd543d2c095d1844be0b1c63
Marcin <ziewa91@wp.pl>
2021-03-28 23:09:47 +0200

    Added LoginVm

---
+++ CookbookAPI/ViewModels/Account/LoginVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.ViewModels.Account
+{
+    public class LoginVm
+    {
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+    }
+}
