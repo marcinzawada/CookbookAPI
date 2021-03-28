217f62043eb03b28fc00bcd76e63d4ae4e789367
Marcin <ziewa91@wp.pl>
2021-03-28 22:41:47 +0200

    Added validator for RegisterRequest

---
+++ CookbookAPI/Requests/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Requests.Account;
+using FluentValidation;
+
+namespace CookbookAPI.Requests.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator(CookbookDbContext dbContext)
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "That email is taken");
+                    }
+                });
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8)
+                .Matches("[A-Z]").WithMessage("'{PropertyName}' must contain one or more capital letters.")
+                .Matches("[a-z]").WithMessage("'{PropertyName}' must contain one or more lowercase letters.")
+                .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits.")
+                .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]")
+                .WithMessage("'{ PropertyName}' must contain one or more special characters.")
+                .Matches("^[^£# “”]*$")
+                .WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(e => e.Password);
+        }
+    }
+}
