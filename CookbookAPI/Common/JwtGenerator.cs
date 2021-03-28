0cd2e57160ea3799c71dfeaf9ee93456e04a604d
Marcin <ziewa91@wp.pl>
2021-03-29 00:14:31 +0200

    Added Name to JWT

--- CookbookAPI/Common/JwtGenerator.cs
+++ CookbookAPI/Common/JwtGenerator.cs
@@ -23,7 +23,8 @@ namespace CookbookAPI.Common
             var claims = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
 
             };
 
