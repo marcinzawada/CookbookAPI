f862c4e1719de75b461e6daf91a96b51d255d8b2
Marcin <ziewa91@wp.pl>
2021-04-01 19:12:23 +0200

    Added MappingExtensions

---
+++ CookbookAPI/Extensions/MappingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CookbookAPI.Requests;
+using CookbookAPI.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookbookAPI.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize, string sortBy, string sortDirection)
+            => PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize, sortBy, sortDirection);
+
+        public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration)
+            => queryable.ProjectTo<TDestination>(configuration).ToListAsync();
+    }
+}
