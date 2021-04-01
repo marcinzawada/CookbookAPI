a94847cbe0eb8c57b313860b1331b80d68bfd769
Marcin <ziewa91@wp.pl>
2021-04-01 18:53:34 +0200

    Added Getall method

---
+++ CookbookAPI/Controllers/RecipesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Requests.Recipes;
+using CookbookAPI.Services;
+
+namespace CookbookAPI.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipesService _recipesService;
+
+        public RecipesController(RecipesService recipesService)
+        {
+            _recipesService = recipesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] RecipesRequest request)
+        {
+            var vm = await _recipesService.GetAll(request);
+            ;
+            return Ok(vm);
+        }
+    }
+}
