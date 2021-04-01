0d0d467d1c2cabf32a56093c53c4fc7b93e5ba01
Marcin <ziewa91@wp.pl>
2021-04-01 19:22:14 +0200

    Added MappingProfile

--- CookbookAPI/Mappers/MappingProfile.cs
+++ CookbookAPI/Mappers/MappingProfile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using CookbookAPI.DTOs;
 using CookbookAPI.DTOs.MealDB;
 using CookbookAPI.Entities;
 
@@ -17,6 +18,14 @@ namespace CookbookAPI.Mappers
             CreateMap<MealAreaDto, Area>();
 
             CreateMap<MealIngredientDto, Ingredient>();
+
+            CreateMap<Ingredient, IngredientDto>();
+
+            CreateMap<Recipe, RecipeDto>()
+                .ForMember(x => x.User, opt => opt.MapFrom(x => x.User.Name))
+                .ForMember(x => x.Category, opt => opt.MapFrom(x => x.Category.Name))
+                .ForMember(x => x.Area, opt => opt.MapFrom(x => x.Area.Name))
+                .ForMember(x => x.Ingredients, opt => opt.MapFrom(x => x.RecipeIngredients.Select(y => new IngredientDto{Id = y.IngredientId, Description = y.Ingredient.Description, Name = y.Ingredient.Name, Measure = y.Measure})));
         }
     }
 }
