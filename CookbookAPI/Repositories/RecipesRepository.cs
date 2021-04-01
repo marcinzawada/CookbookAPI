50cedd93e71cec85b7058f7cc1b194d711cae09c
Marcin <ziewa91@wp.pl>
2021-04-01 19:21:50 +0200

    Added RecipesRepository

---
+++ CookbookAPI/Repositories/RecipesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Entities;
+
+namespace CookbookAPI.Repositories
+{
+    public class RecipesRepository : BaseRepository<Recipe, CookbookDbContext>
+    {
+        public RecipesRepository(CookbookDbContext context) : base(context)
+        {
+        }
+    }
+}
