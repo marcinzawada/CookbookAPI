3d3f6e1db19aab70216d6fdbbdfde0ed4536dfd8
Marcin <ziewa91@wp.pl>
2021-04-01 18:54:36 +0200

    Added base RequestOptions

---
+++ CookbookAPI/Requests/RequestOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Requests
+{
+    public class RequestOptions
+    {
+        public string SearchPhrase { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string SortDirection { get; set; }
+    }
+}
