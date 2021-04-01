90bfc25815266e140db2fe9298c936c663bf785a
Marcin <ziewa91@wp.pl>
2021-04-01 19:00:58 +0200

    Added GetRecipesRequest

---
+++ CookbookAPI/Requests/Recipes/GetRecipesRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Requests.Recipes
+{
+    public class GetRecipesRequest : RequestOptions
+    {
+    }
+}
