39c40f3a30fe9d773c0387361412171a75824e47
Marcin <ziewa91@wp.pl>
2021-04-01 18:54:12 +0200

    Added SortDirection

---
+++ CookbookAPI/Requests/SortDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Requests
+{
+    public class SortDirection
+    {
+        public static string ASC { get; } = "asc";
+
+        public static string DESC { get; } = "desc";
+    }
+}
