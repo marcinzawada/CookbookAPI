1c160209446acb75967a7b8b04cfa5f78c525c42
Marcin <ziewa91@wp.pl>
2021-04-03 18:37:07 +0200

    Added GetRecipeWithDetails

--- CookbookAPI/Repositories/RecipesRepository.cs
+++ CookbookAPI/Repositories/RecipesRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CookbookAPI.Data;
 using CookbookAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CookbookAPI.Repositories
 {
@@ -11,6 +12,18 @@ namespace CookbookAPI.Repositories
     {
         public RecipesRepository(CookbookDbContext context) : base(context)
         {
+        }
+
+        public async Task<Recipe> GetRecipeWithDetails(int id)
+        {
+            var recipe = await _context.Recipes
+                .Include(x => x.RecipeIngredients)
+                .ThenInclude(x => x.Ingredient)
+                .Include(x => x.Area)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return recipe;
         }
     }
 }
