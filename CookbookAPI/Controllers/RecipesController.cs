5f344b38ad34880c8914641c37991f82b0b78b06
Marcin <ziewa91@wp.pl>
2021-04-03 18:40:59 +0200

    Added Get method

--- CookbookAPI/Controllers/RecipesController.cs
+++ CookbookAPI/Controllers/RecipesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CookbookAPI.Requests.Recipes;
 using CookbookAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 
 namespace CookbookAPI.Controllers
@@ -22,6 +23,7 @@ namespace CookbookAPI.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         [Description("Get all recipes")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -29,6 +31,18 @@ namespace CookbookAPI.Controllers
         {
             var vm = await _recipesService.GetAll(request);
             
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        [Description("Get specifed recipe")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var vm = await _recipesService.GetById(id);
+
             return Ok(vm);
         }
     }
