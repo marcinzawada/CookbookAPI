2f384ff54517b3990a5f51c95c12a340b5c8937e
Marcin <ziewa91@wp.pl>
2021-04-06 17:33:11 +0200

    Added RecipeRequest

---
+++ CookbookAPI/DTOs/Recipes/RecipeIngredientDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.DTOs.Recipes
+{
+    public class RecipeIngredientDto
+    {
+        public int IngredientId { get; set; }
+
+        public string Measure { get; set; }
+    }
+}
--- CookbookAPI/Mappers/MappingProfile.cs
+++ CookbookAPI/Mappers/MappingProfile.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using CookbookAPI.DTOs;
 using CookbookAPI.DTOs.MealDB;
 using CookbookAPI.Entities;
+using CookbookAPI.Requests.Recipes;
 
 namespace CookbookAPI.Mappers
 {
@@ -25,7 +26,14 @@ namespace CookbookAPI.Mappers
                 .ForMember(x => x.User, opt => opt.MapFrom(x => x.User.Name))
                 .ForMember(x => x.Category, opt => opt.MapFrom(x => x.Category.Name))
                 .ForMember(x => x.Area, opt => opt.MapFrom(x => x.Area.Name))
-                .ForMember(x => x.Ingredients, opt => opt.MapFrom(x => x.RecipeIngredients.Select(y => new IngredientDto{Id = y.IngredientId, Description = y.Ingredient.Description, Name = y.Ingredient.Name, Measure = y.Measure})));
+                .ForMember(x => x.Ingredients, opt => opt.MapFrom(x => x.RecipeIngredients.Select(y => new IngredientDto { Id = y.IngredientId, Description = y.Ingredient.Description, Name = y.Ingredient.Name, Measure = y.Measure })));
+
+            CreateMap<RecipeRequest, Recipe>()
+                .ForMember(x => x.RecipeIngredients, opt =>
+                        opt.MapFrom(x => x.Ingredients.Select(y => new RecipeIngredient
+                        { IngredientId = y.IngredientId, Measure = y.Measure })))
+                .ForMember(x => x.CreatedAt, opt =>
+                    opt.MapFrom(y => DateTime.UtcNow));
         }
     }
 }
