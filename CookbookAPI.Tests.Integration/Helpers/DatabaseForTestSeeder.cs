27b21a6a68bdfedd2d211abc64377e275dceb493
Marcin <ziewa91@wp.pl>
2021-04-10 13:06:09 +0200

    Added names for users

--- CookbookAPI.Tests.Integration/Helpers/DatabaseForTestSeeder.cs
+++ CookbookAPI.Tests.Integration/Helpers/DatabaseForTestSeeder.cs
@@ -66,7 +66,7 @@ namespace CookbookAPI.Tests.Integration.Helpers
                 },
                 new Ingredient
                 {
-                    Name = "Tomato"
+                    Name = "Potato"
                 }
             };
 
