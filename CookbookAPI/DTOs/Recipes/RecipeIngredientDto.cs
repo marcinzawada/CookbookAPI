80ad14d3661a11375b37d82ad9d53132f095a688
Marcin <ziewa91@wp.pl>
2021-04-13 00:02:48 +0200

    Added required to IngredientId and Measure

--- CookbookAPI/DTOs/Recipes/RecipeIngredientDto.cs
+++ CookbookAPI/DTOs/Recipes/RecipeIngredientDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace CookbookAPI.DTOs.Recipes
 {
     public class RecipeIngredientDto
     {
+        [Required]
         public int IngredientId { get; set; }
 
+        [Required]
         public string Measure { get; set; }
     }
 }
