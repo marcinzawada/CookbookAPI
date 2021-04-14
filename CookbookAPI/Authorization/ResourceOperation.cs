64ac7f1230a90e7441fbccb63820f9d4c3bdb7d3
Marcin <ziewa91@wp.pl>
2021-04-14 12:59:16 +0200

    Added ResourceOperation

---
+++ CookbookAPI/Authorization/ResourceOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Authorization
+{
+    public enum ResourceOperation
+    {
+        Create,
+        Read,
+        Update,
+        Delete
+    }
+}
