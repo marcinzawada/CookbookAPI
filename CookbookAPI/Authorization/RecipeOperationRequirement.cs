726b73321ba5287ace65b408e008aa218aa19b88
Marcin <ziewa91@wp.pl>
2021-04-14 12:58:51 +0200

    Added RecipeOperationRequirement

---
+++ CookbookAPI/Authorization/RecipeOperationRequirement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CookbookAPI.Authorization
+{
+    public class RecipeOperationRequirement : IAuthorizationRequirement
+    {
+        public RecipeOperationRequirement(ResourceOperation resourceOperation)
+        {
+            ResourceOperation = resourceOperation;
+        }
+        public ResourceOperation ResourceOperation { get; }
+    }
+}
