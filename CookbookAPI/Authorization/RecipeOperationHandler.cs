f328692e25242e16216d582fdf22ae9e826ac722
Marcin <ziewa91@wp.pl>
2021-04-14 12:58:05 +0200

    Added RecipeOperationHandler

---
+++ CookbookAPI/Authorization/RecipeOperationHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CookbookAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CookbookAPI.Authorization
+{
+    public class RecipeOperationHandler : AuthorizationHandler<RecipeOperationRequirement, Recipe>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RecipeOperationRequirement requirement,
+            Recipe recipe)
+        {
+            if (requirement.ResourceOperation == ResourceOperation.Read ||
+                requirement.ResourceOperation == ResourceOperation.Create)
+            {
+                context.Succeed(requirement);
+            }
+
+            var userId = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            if (recipe.UserId == int.Parse(userId))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
