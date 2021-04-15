29c1c72b39061abeceaea7db02def2141f1ff24d
Marcin <ziewa91@wp.pl>
2021-04-15 16:22:41 +0200

    Added IRecipesService

---
+++ CookbookAPI/Services/Interfaces/IRecipesService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.DTOs;
+using CookbookAPI.Requests.Recipes;
+using CookbookAPI.ViewModels;
+using CookbookAPI.ViewModels.Recipes;
+
+namespace CookbookAPI.Services.Interfaces
+{
+    public interface IRecipesService
+    {
+        public Task<PaginatedList<RecipeDto>> GetAll(GetRecipesRequest request);
+
+        public Task<GetRecipeVm> GetById(int id);
+
+        public Task<int> Create(RecipeRequest request);
+
+        public Task Update(int id, RecipeRequest request);
+
+        public Task Delete(int id);
+    }
+}
