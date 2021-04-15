dbe5025aaab1e38f3c12638e63b09caaa3512133
Marcin <ziewa91@wp.pl>
2021-04-15 14:03:23 +0200

    Added IEquatable interface

--- CookbookAPI/Entities/RecipeIngredient.cs
+++ CookbookAPI/Entities/RecipeIngredient.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CookbookAPI.Entities
 {
-    public class RecipeIngredient
+    public class RecipeIngredient : IEquatable<RecipeIngredient>
     {
         public virtual Recipe Recipe { get; set; }
 
@@ -16,5 +16,25 @@ namespace CookbookAPI.Entities
         public int IngredientId { get; set; }
 
         public string Measure { get; set; }
+
+        public bool Equals(RecipeIngredient other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return RecipeId == other.RecipeId && IngredientId == other.IngredientId && Measure == other.Measure;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((RecipeIngredient) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RecipeId, IngredientId, Measure);
+        }
     }
 }
