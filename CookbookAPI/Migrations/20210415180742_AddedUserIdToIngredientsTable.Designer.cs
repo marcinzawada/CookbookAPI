6e5d7eab5cf5019285796d11363bc9f28f6fca64
Marcin <ziewa91@wp.pl>
2021-04-17 12:25:21 +0200

    Added UsrerId to Ingredient entity

--- CookbookAPI/Migrations/20210415180742_AddedUserIdToIngredientsTable.Designer.cs
+++ CookbookAPI/Migrations/20210415180742_AddedUserIdToIngredientsTable.Designer.cs
@@ -4,14 +4,16 @@ using CookbookAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookbookAPI.Migrations
 {
     [DbContext(typeof(CookbookDbContext))]
-    partial class CookbookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210415180742_AddedUserIdToIngredientsTable")]
+    partial class AddedUserIdToIngredientsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CookbookAPI/Migrations/20210415180742_AddedUserIdToIngredientsTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookbookAPI.Migrations
+{
+    public partial class AddedUserIdToIngredientsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Ingredients",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_UserId",
+                table: "Ingredients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Users_UserId",
+                table: "Ingredients",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Users_UserId",
+                table: "Ingredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredients_UserId",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Ingredients");
+        }
+    }
+}
