8b8b5fc0d5885905e22379287bc5c6265b337307
Marcin <ziewa91@wp.pl>
2021-04-17 12:40:33 +0200

    Added GetIngredientsRequest

---
+++ CookbookAPI/Requests/Ingredients/GetIngredientsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Requests.Ingredients
+{
+    public class GetIngredientsRequest : RequestOptions
+    {
+    }
+}
