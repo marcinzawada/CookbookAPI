08ec7d75b72c8375932390389d00a7d24e29b3b7
Marcin <ziewa91@wp.pl>
2021-04-17 14:21:36 +0200

    Added BaseService

---
+++ CookbookAPI/Services/BaseService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CookbookAPI.Data;
+using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CookbookAPI.Services
+{
+    public class BaseService
+    {
+        protected readonly CookbookDbContext _context;
+        protected readonly IMapper _mapper;
+        protected readonly IUserContextService _userContextService;
+        protected readonly IAuthorizationService _authorizationService;
+
+        public BaseService(CookbookDbContext context, IMapper mapper, IUserContextService userContextService, IAuthorizationService authorizationService)
+        {
+            _context = context;
+            _mapper = mapper;
+            _userContextService = userContextService;
+            _authorizationService = authorizationService;
+        }
+    }
+}
