90b8d7586011d77ab15a4dd0adf6b92ba8220189
Marcin <ziewa91@wp.pl>
2021-04-17 12:40:04 +0200

    Added IngredientsController

---
+++ CookbookAPI/Controllers/IngredientsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookAPI.Controllers
+{
+    public class IngredientsController : ControllerBase
+    {
+        private readonly IIngredientsService _ingredientsService;
+
+        public IngredientsController(IIngredientsService ingredientsService)
+        {
+            _ingredientsService = ingredientsService;
+        }
+    }
+}
