5f22e2f63cd67141c63bc87a82e1ee62b3092d51
Marcin <ziewa91@wp.pl>
2021-04-17 12:19:54 +0200

    Added AuthenticationInstaller

---
+++ CookbookAPI/Extensions/StartupInstallers/AuthenticationInstaller.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using CookbookAPI.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CookbookAPI.Extensions.StartupInstallers
+{
+    public static class AuthenticationInstaller
+    {
+        public static void InstallAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var authenticationSettings = new AuthenticationSettings();
+
+            configuration.GetSection("Authentication").Bind(authenticationSettings);
+            services.AddSingleton(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+                };
+            });
+        }
+    }
+}
