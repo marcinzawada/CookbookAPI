6ac3b0c514a652c9c63ba28ada866f2065b05008
Marcin <ziewa91@wp.pl>
2021-04-19 19:52:13 +0200

    Added StringExtensions

---
+++ CookbookAPI/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool ContainsIgnoreCase(this string source, string compared)
+        {
+            if (string.IsNullOrEmpty(source))
+                return false;
+
+            return source.Contains(compared, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
