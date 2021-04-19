c4056be4d2db3d8e4e3dde4cc9ead855d8ef2965
Marcin <ziewa91@wp.pl>
2021-04-19 19:51:53 +0200

    Added GetAll Method

--- CookbookAPI/Controllers/IngredientsController.cs
+++ CookbookAPI/Controllers/IngredientsController.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using CookbookAPI.Requests.Ingredients;
 using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookbookAPI.Controllers
 {
+    [Route("api/ingredients")]
+    [ApiController]
     public class IngredientsController : ControllerBase
     {
         private readonly IIngredientsService _ingredientsService;
@@ -14,6 +20,18 @@ namespace CookbookAPI.Controllers
         public IngredientsController(IIngredientsService ingredientsService)
         {
             _ingredientsService = ingredientsService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Description("Get all ingredients")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetAll([FromQuery] GetIngredientsRequest request)
+        {
+            var vm = await _ingredientsService.GetAll(request);
+
+            return Ok(vm);
         }
     }
 }
