14d6d994aeab3b8fd7cc563f1c0440696515c04e
Marcin <ziewa91@wp.pl>
2021-04-20 14:52:02 +0200

    Added GetIngredientVm

---
+++ CookbookAPI/ViewModels/Ingredients/GetIngredientVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.DTOs;
+
+namespace CookbookAPI.ViewModels.Ingredients
+{
+    public class GetIngredientVm
+    {
+        public IngredientDto Ingredient { get; set; }
+    }
+}
