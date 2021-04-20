0d2995fec12c3a0af47c4366bc78826ade6d44bb
Marcin <ziewa91@wp.pl>
2021-04-20 14:41:29 +0200

    Fixed interface methods

--- CookbookAPI/Services/Interfaces/IIngredietnsService.cs
+++ CookbookAPI/Services/Interfaces/IIngredietnsService.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CookbookAPI.DTOs;
+using CookbookAPI.Requests.Ingredients;
 using CookbookAPI.Requests.Recipes;
 using CookbookAPI.ViewModels;
+using CookbookAPI.ViewModels.Ingredients;
 using CookbookAPI.ViewModels.Recipes;
 
 namespace CookbookAPI.Services.Interfaces
 {
     public interface IIngredientsService
     {
-        public Task<PaginatedList<IngredientDto>> GetAll(GetRecipesRequest request);
+        public Task<PaginatedList<IngredientDto>> GetAll(GetIngredientsRequest request);
 
-        public Task<GetRecipeVm> GetById(int id);
+        public Task<GetIngredientVm> GetById(int id);
 
-        public Task<int> Create(RecipeRequest request);
+        public Task<int> Create(IngredientRequest request);
 
-        public Task Update(int id, RecipeRequest request);
+        public Task Update(int id, IngredientRequest request);
 
         public Task Delete(int id);
     }
