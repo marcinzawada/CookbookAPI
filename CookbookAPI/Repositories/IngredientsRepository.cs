f6a3f1d978b3644d6dd50761467141f1d9249a34
Marcin <ziewa91@wp.pl>
2021-04-24 18:39:36 +0200

    Added IngredientsRepository

---
+++ CookbookAPI/Repositories/IngredientsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Entities;
+using CookbookAPI.Repositories.Interfaces;
+
+namespace CookbookAPI.Repositories
+{
+    public class IngredientsRepository : BaseRepository<Ingredient, CookbookDbContext>, IIngredientsRepository<Ingredient>
+    {
+        public IngredientsRepository(CookbookDbContext context) : base(context)
+        {
+        }
+    }
+}
