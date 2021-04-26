574f86bb6167b9fd2f4affd00e7f531dd02cda52
Marcin <ziewa91@wp.pl>
2021-04-26 21:29:29 +0200

    Added IAreasService

---
+++ CookbookAPI/Services/Interfaces/IAreasService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.ViewModels.Areas;
+
+namespace CookbookAPI.Services.Interfaces
+{
+    public interface IAreasService
+    {
+        public Task<GetAreaVm> GetAll();
+    }
+}
