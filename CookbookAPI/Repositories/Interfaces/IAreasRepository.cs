0d59a57b6d982cecc7f83ec956b7571d727f4c4a
Marcin <ziewa91@wp.pl>
2021-04-26 21:23:50 +0200

    Added IAreasRepository

---
+++ CookbookAPI/Repositories/Interfaces/IAreasRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Entities;
+
+namespace CookbookAPI.Repositories.Interfaces
+{
+    public interface IAreasRepository<TArea> : IBaseRepository<Area>
+    {
+    }
+}
