67769dd96921d554e416684460f79721a40ccdaf
Marcin <ziewa91@wp.pl>
2021-04-26 14:43:45 +0200

    Deleted IngredientDto, added Name, Description, Recipes

--- CookbookAPI/ViewModels/Ingredients/GetIngredientVm.cs
+++ CookbookAPI/ViewModels/Ingredients/GetIngredientVm.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CookbookAPI.DTOs;
+using CookbookAPI.DTOs.Ingredients;
 
 namespace CookbookAPI.ViewModels.Ingredients
 {
     public class GetIngredientVm
     {
-        public IngredientDto Ingredient { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<IngredientRecipeDto> Recipes { get; set; } = new List<IngredientRecipeDto>();
     }
 }
