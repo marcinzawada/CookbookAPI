397f8358b9dee18a224f43cbd52b210c286a724b
Marcin <ziewa91@wp.pl>
2021-04-26 20:07:49 +0200

    Added IngredientOperationRequirement

---
+++ CookbookAPI/Authorization/IngredientOperationRequirement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CookbookAPI.Authorization
+{
+    public class IngredientOperationRequirement : IAuthorizationRequirement
+    {
+        public IngredientOperationRequirement(ResourceOperation resourceOperation)
+        {
+            ResourceOperation = resourceOperation;
+        }
+        public ResourceOperation ResourceOperation { get; }
+    }
+}
