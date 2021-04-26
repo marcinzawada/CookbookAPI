2601990202ef3c07c0c6e25efdef97ca10564f62
Marcin <ziewa91@wp.pl>
2021-04-26 21:23:18 +0200

    Added GetAll method

---
+++ CookbookAPI/Controllers/AreasController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Requests.Recipes;
+using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookAPI.Controllers
+{
+    [Route("api/areas")]
+    [ApiController]
+    public class AreasController : ControllerBase
+    {
+        private readonly IAreasService _areasService;
+
+        public AreasController(IAreasService areasService)
+        {
+            _areasService = areasService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Description("Get all areas")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            var vm = await _areasService.GetAll();
+
+            return Ok(vm);
+        }
+
+    }
+}
