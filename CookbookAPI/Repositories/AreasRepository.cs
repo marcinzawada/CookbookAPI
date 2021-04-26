8c8acbaf8667da7f24d665aa95053d5d245a48c1
Marcin <ziewa91@wp.pl>
2021-04-26 21:24:19 +0200

    Added AreasRepository

---
+++ CookbookAPI/Repositories/AreasRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Entities;
+using CookbookAPI.Repositories.Interfaces;
+
+namespace CookbookAPI.Repositories
+{
+    public class AreasRepository : BaseRepository<Area, CookbookDbContext>, IAreasRepository<Area>
+    {
+        public AreasRepository(CookbookDbContext context) : base(context)
+        {
+        }
+    }
+}
