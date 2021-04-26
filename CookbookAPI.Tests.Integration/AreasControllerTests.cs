25f00dca2bf1ac1ee8f8ca5a635e482a77139191
Marcin <ziewa91@wp.pl>
2021-04-26 21:30:08 +0200

    Added tests for GetAll method

---
+++ CookbookAPI.Tests.Integration/AreasControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CookbookAPI.ViewModels.Areas;
+using FluentAssertions;
+using Xunit;
+
+namespace CookbookAPI.Tests.Integration
+{
+    public class AreasControllerTests : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutValidAuthorizeHeader_ShouldReturnUnauthorized()
+        {
+            //Arrange
+
+            //Act
+            var response = await _testClient.GetAsync("/api/areas");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+
+        public async Task GetAll_WithValidAuthorizeHeader_ShouldReturnAreas()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync("/api/areas");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<GetAreaVm>()).Areas.Count.Should().BeGreaterOrEqualTo(1);
+        }
+    }
+}
