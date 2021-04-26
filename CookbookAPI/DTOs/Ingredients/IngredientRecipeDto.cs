0d738605b9b9be10a7460596adb8c964d17953cd
Marcin <ziewa91@wp.pl>
2021-04-26 14:37:28 +0200

    Added IngredientRecipeDto

---
+++ CookbookAPI/DTOs/Ingredients/IngredientRecipeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.DTOs.Ingredients
+{
+    public class IngredientRecipeDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Thumbnail { get; set; }
+    }
+}
