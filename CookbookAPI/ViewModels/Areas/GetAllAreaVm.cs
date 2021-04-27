4b6b3b295d92935b42782177d5223e93c221f7d4
Marcin <ziewa91@wp.pl>
2021-04-27 13:42:17 +0200

    Added GetAllAreaVm

---
+++ CookbookAPI/ViewModels/Areas/GetAllAreaVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.DTOs;
+
+namespace CookbookAPI.ViewModels.Areas
+{
+    public class GetAllAreaVm
+    {
+        public List<AreaDto> Areas { get; set; }
+    }
+}
