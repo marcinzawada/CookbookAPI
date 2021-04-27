4f01e06f20cb691c60fa77ff28d182ef289d1b4a
Marcin <ziewa91@wp.pl>
2021-04-27 15:27:04 +0200

    Added CategoryService, added GetAll method

---
+++ CookbookAPI/Services/CategoriesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CookbookAPI.Data;
+using CookbookAPI.DTOs;
+using CookbookAPI.Entities;
+using CookbookAPI.Repositories.Interfaces;
+using CookbookAPI.Services.Interfaces;
+using CookbookAPI.ViewModels.Areas;
+using CookbookAPI.ViewModels.Categories;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CookbookAPI.Services
+{
+    public class CategoriesService : BaseService, ICategoriesService
+    {
+        private readonly ICategoriesRepository<Category> _categoriesRepository;
+
+        public CategoriesService(CookbookDbContext context, IMapper mapper,
+            IUserContextService userContextService, IAuthorizationService authorizationService,
+            ICategoriesRepository<Category> categoriesRepository) 
+            : base(context, mapper, userContextService, authorizationService)
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+
+        public async Task<GetAllCategoriesVm> GetAll()
+        {
+            var categories = await _categoriesRepository.GetAll();
+
+            var categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
+
+            return new GetAllCategoriesVm
+            {
+                Categories = categoryDtos
+            };
+        }
+    }
+}
