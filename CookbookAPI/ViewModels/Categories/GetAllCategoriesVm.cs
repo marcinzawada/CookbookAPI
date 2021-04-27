e2f6d7132a85b5112009bbe92bfee40d44083875
Marcin <ziewa91@wp.pl>
2021-04-27 15:25:48 +0200

    Added GetallCategoriesVm

---
+++ CookbookAPI/ViewModels/Categories/GetAllCategoriesVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.DTOs;
+
+namespace CookbookAPI.ViewModels.Categories
+{
+    public class GetAllCategoriesVm
+    {
+        public List<CategoryDto> Categories { get; set; } = new List<CategoryDto>();
+    }
+}
