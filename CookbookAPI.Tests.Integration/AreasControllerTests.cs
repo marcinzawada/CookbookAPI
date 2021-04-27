f9b6fc64e725773c4b7afbaf25533fbcb45b7c90
Marcin <ziewa91@wp.pl>
2021-04-27 13:48:47 +0200

    Added tests for Get method

--- CookbookAPI.Tests.Integration/AreasControllerTests.cs
+++ CookbookAPI.Tests.Integration/AreasControllerTests.cs
@@ -25,7 +25,6 @@ namespace CookbookAPI.Tests.Integration
         }
 
         [Fact]
-
         public async Task GetAll_WithValidAuthorizeHeader_ShouldReturnAreas()
         {
             //Arrange
@@ -36,7 +35,39 @@ namespace CookbookAPI.Tests.Integration
 
             //Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await response.Content.ReadAsAsync<GetAreaVm>()).Areas.Count.Should().BeGreaterOrEqualTo(1);
+            (await response.Content.ReadAsAsync<GetAllAreaVm>()).Areas.Count.Should().BeGreaterOrEqualTo(1);
+        }
+
+        [Fact]
+        public async Task Get_WithValidAreaId_ShouldReturnAreaWithRecipes()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync("/api/areas/1/recipes");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var body = await response.Content.ReadAsAsync<GetAreaVm>();
+            body.Name.Should().NotBeNullOrEmpty();
+            body.Id.Should().BeGreaterOrEqualTo(1);
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        [InlineData(0)]
+        public async Task Get_InvalidAreaId_ShouldReturnNotFound(int id)
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync($"/api/areas/{id}/recipes");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
     }
 }
