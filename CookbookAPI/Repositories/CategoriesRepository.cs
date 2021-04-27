a0ffd574c2f2fb8d5a31bc8e6e6bf297db586566
Marcin <ziewa91@wp.pl>
2021-04-27 15:27:54 +0200

    Added CategoriesRepository

---
+++ CookbookAPI/Repositories/CategoriesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Data;
+using CookbookAPI.Entities;
+using CookbookAPI.Repositories.Interfaces;
+
+namespace CookbookAPI.Repositories
+{
+    public class CategoriesRepository : BaseRepository<Category, CookbookDbContext>, ICategoriesRepository<Category>
+    {
+        public CategoriesRepository(CookbookDbContext context) : base(context)
+        {
+        }
+    }
+}
