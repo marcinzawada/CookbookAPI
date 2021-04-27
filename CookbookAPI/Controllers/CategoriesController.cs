3f96a56e417c00e13f2a0118966e68059a527a07
Marcin <ziewa91@wp.pl>
2021-04-27 15:23:42 +0200

    Added GetAll method

---
+++ CookbookAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookbookAPI.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoriesService _categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            _categoriesService = categoriesService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Description("Get all categoties")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            var vm = await _categoriesService.GetAll();
+
+            return Ok(vm);
+        }
+    }
+}
