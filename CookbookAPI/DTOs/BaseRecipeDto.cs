eddb8896e2cf3722529137c5d95751c5eb6aaa9f
Marcin <ziewa91@wp.pl>
2021-04-27 13:13:07 +0200

    Added BaseRecipeDto

---
+++ CookbookAPI/DTOs/BaseRecipeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.DTOs
+{
+    public class BaseRecipeDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Thumbnail { get; set; }
+    }
+}
