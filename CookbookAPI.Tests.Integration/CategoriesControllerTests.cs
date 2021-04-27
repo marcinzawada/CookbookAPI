9d2db314eca02fbd43c28954d0a5b3655f0d82d2
Marcin <ziewa91@wp.pl>
2021-04-27 15:28:09 +0200

    Added tests for GetAll method

---
+++ CookbookAPI.Tests.Integration/CategoriesControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CookbookAPI.ViewModels.Areas;
+using CookbookAPI.ViewModels.Categories;
+using FluentAssertions;
+using Xunit;
+
+namespace CookbookAPI.Tests.Integration
+{
+    public class CategoriesControllerTests : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutValidAuthorizeHeader_ShouldReturnUnauthorized()
+        {
+            //Arrange
+
+            //Act
+            var response = await _testClient.GetAsync("/api/categories");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task GetAll_WithValidAuthorizeHeader_ShouldReturnCategories()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync("/api/categories");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<GetAllCategoriesVm>()).Categories.Count.Should().BeGreaterOrEqualTo(1);
+        }
+    }
+}
