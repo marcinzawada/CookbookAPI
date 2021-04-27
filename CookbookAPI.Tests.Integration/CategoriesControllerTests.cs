32bca207c582801e1c8a366e9017ef8ef79a5c71
Marcin <ziewa91@wp.pl>
2021-04-27 19:05:40 +0200

    Added tests for Get method

--- CookbookAPI.Tests.Integration/CategoriesControllerTests.cs
+++ CookbookAPI.Tests.Integration/CategoriesControllerTests.cs
@@ -38,5 +38,37 @@ namespace CookbookAPI.Tests.Integration
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             (await response.Content.ReadAsAsync<GetAllCategoriesVm>()).Categories.Count.Should().BeGreaterOrEqualTo(1);
         }
+
+        [Fact]
+        public async Task Get_WithValidCategoryId_ShouldReturnCategoryWithRecipes()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync("/api/categories/1/recipes");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var body = await response.Content.ReadAsAsync<GetCategoryVm>();
+            body.Name.Should().NotBeNullOrEmpty();
+            body.Id.Should().BeGreaterOrEqualTo(1);
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        [InlineData(0)]
+        public async Task Get_InvalidCategoryId_ShouldReturnNotFound(int id)
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await _testClient.GetAsync($"/api/categories/{id}/recipes");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
     }
 }
