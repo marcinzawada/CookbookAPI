9f5082b9b6eec001ddcc1f7786654cde14cfcbc4
Marcin <ziewa91@wp.pl>
2021-04-27 13:12:48 +0200

    Added relation to recipes

--- CookbookAPI/Entities/Area.cs
+++ CookbookAPI/Entities/Area.cs
@@ -12,5 +12,7 @@ namespace CookbookAPI.Entities
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public List<Recipe> Recipes { get; set; } = new List<Recipe>();
     }
 }
