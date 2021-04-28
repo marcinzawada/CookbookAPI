10ba6449d67210ad673c95c06a276977fe12c065
Marcin <ziewa91@wp.pl>
2021-04-28 14:17:57 +0200

    Added UserFavoriteRecipe

---
+++ CookbookAPI/Entities/UserFavoriteRecipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookbookAPI.Entities
+{
+    public class UserFavoriteRecipe
+    {
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        public int RecipeId { get; set; }
+
+        [ForeignKey(nameof(RecipeId))]
+        public Recipe Recipe { get; set; }
+    }
+}
