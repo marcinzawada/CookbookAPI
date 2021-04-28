f026a6473e30c42c7205d15c5a567e6527a3df30
Marcin <ziewa91@wp.pl>
2021-04-28 14:26:27 +0200

    Added GetAllFavoriteRecipesVm

---
+++ CookbookAPI/ViewModels/Recipes/GetAllFavoriteRecipesVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookbookAPI.DTOs;
+
+namespace CookbookAPI.ViewModels.Recipes
+{
+    public class GetAllFavoriteRecipesVm
+    {
+        public List<BaseRecipeDto> FavoriteRecipes { get; set; } = new List<BaseRecipeDto>();
+    }
+}
