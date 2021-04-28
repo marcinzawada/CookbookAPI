6642c8ec1101383d4d4d3665dc93ba4fac7fe235
Marcin <ziewa91@wp.pl>
2021-04-28 14:17:47 +0200

    Added UserFavoriteRecipe table

---
+++ CookbookAPI/Migrations/20210427182314_AddedUserFavoriteRecipeTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookbookAPI.Migrations
+{
+    public partial class AddedUserFavoriteRecipeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFavoriteRecipes",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    RecipeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavoriteRecipes", x => new { x.RecipeId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserFavoriteRecipes_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFavoriteRecipes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavoriteRecipes_UserId",
+                table: "UserFavoriteRecipes",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFavoriteRecipes");
+        }
+    }
+}
